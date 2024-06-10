d65183ce28d7450df23ef6dd113bef87e0825cca
BAEK MIJIN <128350820+mijinB@users.noreply.github.com>
2024-06-10 06:48:13 +0000

    검색한 keyword로 검색한 jobs 추출 및 params로 보내기 & html 파일에서 python 코드 사용하기

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+from extractors.remoteok import get_jobs
 
 app = Flask("JobScrapper")
+
 
 @app.route("/")
 def home():
   return render_template("home.html", name="nico")
 
+
+db = {}
+
+
 @app.route("/search")
 def hello():
   keyword = request.args.get("keyword")
-  return render_template("search.html", keyword=keyword)
+  if keyword == None:
+    return redirect("/") 
+  if keyword in db:
+    jobs = db[keyword]
+  else:
+    jobs = get_jobs(keyword)
+    db[keyword] = jobs
+
+  return render_template("search.html", keyword=keyword, jobs=jobs)
+
 
 app.run("0.0.0.0")
